28b60916976307b5ab64479bac085d9ac363dda3
Yann Klein <yann.klein@me.com>
2024-05-17 13:44:46 +0900

    modify db setup and style

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,12 @@
 # And connect to the database
 config_path = File.join(__dir__, "database.yml")
 ActiveRecord::Base.configurations = YAML.load_file(config_path)
-ActiveRecord::Base.establish_connection(:environment)
+configure :development do
+  ActiveRecord::Base.establish_connection(:development)
+end
+configure :production do
+  ActiveRecord::Base.establish_connection(:production)
+end
 
 # Load all models!
 Dir["#{__dir__}/../models/*.rb"].each {|file| require file }
