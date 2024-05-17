c50fccef552a930b16e218b6508ad3339fc1f266
Yann Klein <yann.klein@me.com>
2024-05-17 14:14:03 +0900

    simplify connection config

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,9 @@
-# This is some boilerplate code to read the config/database.yml file
-# And connect to the database
-config_path = File.join(__dir__, "database.yml")
-ActiveRecord::Base.configurations = YAML.load_file(config_path)
-configure :development do
-  ActiveRecord::Base.establish_connection(:development)
-end
-configure :production do
-  ActiveRecord::Base.establish_connection(:production)
-end
+puts "Establishing connection with DB"
+ActiveRecord::Base.establish_connection(
+  :adapter  => "sqlite3",
+  :database => "db/development.sqlite3"
+)
+puts "Connection established"
 
 # Load all models!
 Dir["#{__dir__}/../models/*.rb"].each {|file| require file }
